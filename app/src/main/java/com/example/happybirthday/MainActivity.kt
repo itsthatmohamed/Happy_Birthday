6691c5198d7019b0bda9dad2c8c5c7d377199919
Mohamed Hussein <Mohamed.Hussein@sainsburys.co.uk>
2025-03-05 10:27:11 +0000

    Happy-birthday-message

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,8 +32,8 @@ class MainActivity : ComponentActivity() {
                 Box{
                     GreetingImage()
                     GreetingText(
-                        message = "Happy Birthday Charlie!",
-                        from = "from Mohamed",
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -47,12 +49,18 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
+            .fillMaxSize()
+            .wrapContentHeight(align = Alignment.CenterVertically)
     ) {
         Text(
             text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
-            modifier = Modifier.padding(8.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(
+                    /*top = 263.dp*/
+                )
+                .fillMaxWidth(),
             textAlign = TextAlign.Center
         )
 
@@ -62,10 +70,10 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             lineHeight = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End),
+                .align(alignment = Alignment.CenterHorizontally),
 
 
-        )
+            )
     }
 }
 
